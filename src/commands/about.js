a3527f01752603501a5b38b34bff4bd82b93cde9
finnksh <thelicius.s7x@gmail.com>
2023-11-14 21:51:19 +0700

    fix about

--- src/commands/about.js
+++ src/commands/about.js
@@ -5,9 +5,7 @@ module.exports = {
     data : new SlashCommandBuilder()
             .setName("about").setDescription("About Twilight"),
             async execute(interaction){
-                const content = `Twilight is a codename for database located in ██████.
-                It's main function is to store data with code name ██████ as a top secret information.
-                Twilight was built under codename "黄昏 たそがれ" and was built by Ren, Nahiiko, and mad scientist "ICY".
+                const content = `Twilight is a codename for database located in ██████. It's main function is to store data with code name ██████ as a top secret information.Twilight was built under codename "黄昏 たそがれ" and was built by Ren, Nahiiko, and mad scientist "Kira".
                 `;
                 await interaction.reply(content);
             }
